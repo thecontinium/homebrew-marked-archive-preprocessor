5397cca187d4555d597d7f15b4f93cf883c115ab
thecontinium <thecontinium@outlook.com>
2019-02-11 16:25:56 +0800

    initial

--- Formula/marked-archive-preprocessor.rb
+++ Formula/marked-archive-preprocessor.rb
@@ -1,7 +1,7 @@
 class MarkedArchivePreprocessor < Formula
   homepage '  https://github.com/thecontinium/homebrew-marked-archive-preprocessor'
   url 'https://github.com/thecontinium/homebrew-marked-archive-preprocessor.git'
-  version '0.0.1'
+  version '0.0.2'
 
   skip_clean 'bin'
 
